924430c980797ec52ab8994b8766886bb498aa0b
SHIMOMURA Sho <graueule@gmail.com>
2022-08-04 10:10:09 +0900

    first commit

---
+++ src/serial.rs
@@ -0,0 +1,33 @@
+use uart_16550::SerialPort;
+use spin::Mutex;
+use lazy_static::lazy_static;
+
+// lazy_static で囲って init が実行時に1度だけ呼ばれるようにする
+lazy_static! {
+    pub static ref SERIAL1: Mutex<SerialPort> = {
+        // qemu ではシリアルポートはポートアドレスの 0x3f8 にマップされる
+        let mut serial_port = unsafe { SerialPort::new(0x3f8) };
+        serial_port.init();
+        Mutex::new(serial_port)
+    };
+}
+
+#[doc(hidden)]
+pub fn _print(args: ::core::fmt::Arguments) {
+    use core::fmt::Write;
+    SERIAL1.lock().write_fmt(args).expect("Printing to serial failed.");
+}
+
+#[macro_export]
+macro_rules! serial_print {
+    ($($arg:tt)*) => {
+        $crate::serial::_print(format_args!($($arg)*));
+    };
+}
+
+#[macro_export]
+macro_rules! serial_println {
+    () => ($crate::serial_print!("\n"));
+    ($fmt:expr) => ($crate::serial_print!(concat!($fmt, "\n")));
+    ($fmt:expr, $($arg:tt)*) => ($crate::serial_print!(concat!($fmt, "\n"), $($arg)*))
+}
