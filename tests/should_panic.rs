1c41464696224da9417a9147488d1b901cb42314
SHIMOMURA Sho <graueule@gmail.com>
2022-08-04 12:09:15 +0900

    remove test runner

--- tests/should_panic.rs
+++ tests/should_panic.rs
@@ -1,39 +1,26 @@
 #![no_std]
 #![no_main]
-#![feature(custom_test_frameworks)]
-#![test_runner(test_runner)]
-#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 use blog_os::{QemuExitCode, exit_qemu, serial_println, serial_print};
 
-// panic したらそこで qemu が終了するのでテストが1回成功したら終わり(2個目以降は無視される)
+// どうせ1個しかテストを実行できないのでハーネスを無効化
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    should_fail();
+    serial_println!("[test did not panic]");
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
+
+fn should_fail() {
+    serial_print!("should_panic::should_fail...\t");
+    assert_eq!(1, 0);
+}
+
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     serial_println!("[ok]");
     exit_qemu(QemuExitCode::Success);
     loop {}
-}
-
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    test_main();
-    loop {}
-}
-
-pub fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-        serial_println!("[test did not panic]");
-        exit_qemu(QemuExitCode::Failed);
-    }
-    // ここには到達しないはず
-    exit_qemu(QemuExitCode::Success);
-}
-
-#[test_case]
-fn should_fail() {
-    serial_print!("should_panic::should_fail...\t");
-    assert_eq!(1, 0);
 }
