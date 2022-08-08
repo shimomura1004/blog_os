70b0c6356934ec552b817b410548794e6cf726a8
SHIMOMURA Sho <graueule@gmail.com>
2022-08-08 21:30:42 +0900

    virt -> phys

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@
 use bootloader::{entry_point, BootInfo};
 use x86_64::structures::paging::PageTable;
 use core::panic::PanicInfo;
-use blog_os::println;
+use blog_os::{println, memory::translate_addr};
 
 entry_point!(kernel_main);
 
@@ -21,24 +21,37 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     blog_os::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let l4_table = unsafe { active_level_4_table(phys_mem_offset) };
 
-    for (i, entry) in l4_table.iter().enumerate() {
-        if !entry.is_unused() {
-            println!("L4 entry {}: {:?}", i, entry);
+    let addresses = [
+        0xb8000,
+        0x201008,
+        0x0100_0020_1a10,
+    ];
 
-            let phys = entry.frame().unwrap().start_address();
-            let virt = phys.as_u64() + boot_info.physical_memory_offset;
-            let ptr = VirtAddr::new(virt).as_mut_ptr();
-            let l3_table: &PageTable = unsafe { &*ptr };
-
-            for (i, entry) in l3_table.iter().enumerate() {
-                if !entry.is_unused() {
-                    println!("L3 entry {}: {:?}", i, entry);
-                }
-            }
-        }
+    for &address in &addresses {
+        let virt = VirtAddr::new(address);
+        let phys = unsafe { translate_addr(virt, phys_mem_offset) };
+        println!("{:?} -> {:?}", virt, phys);
     }
+
+    // let l4_table = unsafe { active_level_4_table(phys_mem_offset) };
+
+    // for (i, entry) in l4_table.iter().enumerate() {
+    //     if !entry.is_unused() {
+    //         println!("L4 entry {}: {:?}", i, entry);
+
+    //         let phys = entry.frame().unwrap().start_address();
+    //         let virt = phys.as_u64() + boot_info.physical_memory_offset;
+    //         let ptr = VirtAddr::new(virt).as_mut_ptr();
+    //         let l3_table: &PageTable = unsafe { &*ptr };
+
+    //         for (i, entry) in l3_table.iter().enumerate() {
+    //             if !entry.is_unused() {
+    //                 println!("L3 entry {}: {:?}", i, entry);
+    //             }
+    //         }
+    //     }
+    // }
 
     #[cfg(test)]
     test_main();
--- src/memory.rs
+++ src/memory.rs
@@ -1,3 +1,4 @@
+use x86_64::PhysAddr;
 use::x86_64::{
     structures::paging::PageTable,
     VirtAddr,
@@ -18,4 +19,43 @@ pub unsafe fn active_level_4_table(physical_memory_offset: VirtAddr)
     let page_table_ptr: *mut PageTable = virt.as_mut_ptr();
 
     &mut *page_table_ptr
+}
+
+pub unsafe fn translate_addr(addr: VirtAddr, physical_memory_offset: VirtAddr)
+ -> Option<PhysAddr>
+{
+    translate_addr_inner(addr, physical_memory_offset)
+}
+
+fn translate_addr_inner(addr: VirtAddr, physical_memory_offset: VirtAddr)
+ -> Option<PhysAddr>
+{
+    use x86_64::structures::paging::page_table::FrameError;
+    use x86_64::registers::control::Cr3;
+
+    let (level4_table_frame, _) = Cr3::read();
+
+    let table_indexes = [
+        addr.p4_index(), addr.p3_index(), addr.p2_index(), addr.p1_index()
+    ];
+    // この変数が指す先を書き換えながら各レベルのフレームを参照していく
+    let mut frame = level4_table_frame;
+
+    for &index in &table_indexes {
+        // エントリに書かれているのは物理アドレスなので、仮想アドレスに変換する
+        let virt = physical_memory_offset + frame.start_address().as_u64();
+        let table_ptr: *const PageTable = virt.as_ptr();
+        let table = unsafe { &*table_ptr };
+
+        let entry = &table[index];
+        // L1 のインデックスまで調べると、実際のページの物理アドレスが得られる
+        frame = match entry.frame() {
+            Ok(frame) => frame,
+            Err(FrameError::FrameNotPresent) => return None,
+            Err(FrameError::HugeFrame) => panic!("huge pages not supported"),
+        };
+    }
+
+    // オフセットを足して物理アドレスを仮想アドレスにしてから返す
+    Some(frame.start_address() + u64::from(addr.page_offset()))
 }
